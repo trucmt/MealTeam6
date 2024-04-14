d701c05ee025ce1ab0f6a759368c5d325eff7b6e
dearest <51027588+lightoverplay@users.noreply.github.com>
2024-04-14 11:32:12 +0700

    Update clickjackingByMealTeam6.py

--- clickjackingByMealTeam6.py
+++ clickjackingByMealTeam6.py
@@ -1,45 +1,55 @@
-#!/usr/bin/python3
-from urllib.request import Request, urlopen
+from flask import Flask, request, render_template_string, redirect, url_for
 import argparse
-from sys import exit
 import urllib
-import requests
-import urllib.request
-from termcolor import colored
+from urllib.request import Request, urlopen
 from urllib.parse import urlparse
+import os
 import re
+import requests
 
-vuln=False
-parser = argparse.ArgumentParser(
-    description='Scan the website for clickjacking')
-parser.add_argument(
-    "-f", type=str, help="File to store a list of urls", required=True)
+app = Flask(__name__)
 
+HTML = """
+<!doctype html>
+<html>
+<head>
+    <title>Clickjacking Test Tool</title>
+    <link rel="stylesheet" type="text/css" href="/static/style.css">
+</head>
+<body>
+    <h2>Upload File with URLs to Check for Clickjacking</h2>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+</body>
+</html>
+"""
 
-content = parser.parse_args()
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            content = file.read().decode('utf-8')
+            results = check_clickjacking(content)
+            return render_template_string(results)
+    return HTML
 
-d = open(content.f, 'r')
-hdr = {'User-Agent': 'Mozilla/5.0'}
-
-try:
-    for target in d.readlines():
-        t = target.strip('\n')
-        if (("http") or ("https")) not in t:
-            t = "https://"+t  
+def check_clickjacking(content):
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    results = ""
+    for target in content.splitlines():
+        t = target.strip()
+        if not t.startswith(('http://', 'https://')):
+            t = 'https://' + t
         try:
             req = Request(t, headers=hdr)
             data = urlopen(req, timeout=10)
-            response= requests.get(t)
-            filename = urlparse(t).netloc
             headers = data.info()
-            if "Referer" in req.headers:
-                host_url = req.host_url
-                referer_url = req.headers["Referer"]
-                if not referer_url.startswith(host_url):
-                   vuln = True
-            if not (("X-Frame-Options") or ("x-frame-options") or ("Content-Security-Policy") or ("content-security-policy")  or ("frame-ancestors")) in headers:
-                vuln = True
-            
+            response= requests.get(t)
+            vuln= False
+
             frame_buster = re.search(r'<script>(.*)if\(top \!= self\)\s*{', response.text, re.DOTALL | re.IGNORECASE)
             pattern_onbeforeunload = r'<script>\s*window\.onbeforeunload[^<]*</script>.*?<iframe src="([^"]+)"'
             match_pattern_onbeforeunload = re.search(pattern_onbeforeunload, response.text, re.IGNORECASE | re.DOTALL)
@@ -51,6 +61,15 @@ try:
             content_security_policy = response.headers.get('Content-Security-Policy', '')
             
             desired_domain= t
+
+            if "Referer" in req.headers:
+                host_url = req.host_url
+                referer_url = req.headers["Referer"]
+                if not referer_url.startswith(host_url):
+                   vuln = True
+            if not (("X-Frame-Options") or ("x-frame-options") or ("Content-Security-Policy") or ("content-security-policy")  or ("frame-ancestors")) in headers:
+                vuln = True
+
             if match_pattern_onbeforeunload:
                iframe_src = match_pattern_onbeforeunload.group(1)  # Lấy giá trị của src
                if iframe_src.startswith(desired_domain):
@@ -87,62 +106,19 @@ try:
             if x_frame_options:
                 if x_frame_options.lower() == 'allow-from *':
                    print(('red_msg', f'[!] The site may be vulnerable to clickjacking. X-Frame-Options content: {x_frame_options}'))
-                   vuln= True
+                   vuln= True    
 
-            if frame_buster:
-                print(('orange_msg', f'[!] Frame-buster found on the page, but can be bypassed using the \'sandbox=\"allow-forms\"\' attribute.'))
-                print(colored(f"Target: {t} is Vulnerable", "green"))
-                print(colored(f"Generating {filename}.html demo File", "yellow"))
-                demo = """
-                    <html>
-                    <head><title>Clickjack demo page with frame-buster</title></head>
-                    <body>
-                    <p>Website is vulnerable to clickjacking! with frame-buster</p>
-                    <iframe sandbox="allow-forms" src="{}" width="500" height="500"></iframe>
-                    </body>
-                    </html>
-                    """.format(t)
-                if ":" in filename:
-                    url = filename.split(':')
-                    filename=url[0]              
-                with open(filename+".html", "w") as pf:
-                    pf.write(demo)
-                print(colored(f"Clickjacking file Created SuccessFully, Open {filename}.html to get the demo", "blue"))
-            
+            if 'X-Frame-Options' not in headers:
+                results += f"<p style='color: green;'>Target: {t} is Vulnerable</p>"
+            elif frame_buster:
+                print((f"Target: {t} is Vulnerable", "red"))           
             elif vuln ==True:
-                print(colored(f"Target: {t} is Vulnerable", "green"))
-                print(colored(f"Generating {filename}.html demo File", "yellow"))
-                demo = """
-                    <html>
-                    <head><title>Clickjack page</title></head>
-                    <body>
-                    <p>Website is vulnerable to clickjacking!</p>
-                    <iframe src="{}" width="500" height="500"></iframe>
-                    </body>
-                    </html>
-                    """.format(t)
-                if ":" in filename:
-                    url = filename.split(':')
-                    filename=url[0]              
-                with open(filename+".html", "w") as pf:
-                    pf.write(demo)
-                print(colored(f"Clickjacking demo file Created SuccessFully, Open {filename}.html to get the demo", "blue"))
+                print((f"Target: {t} is Vulnerable", "red"))
             else:
-                vuln == False
-                print(colored(f"Target: {t} is not Vulnerable", "red"))
-                print("Testing Other Url's in the List")
-        except KeyboardInterrupt as k:
-            print("No Worries , I'm here to handle your KeyBoard Interrupts \n")
-        except urllib.error.URLError as e:
-            # handling HTTP 403 Forbidden timeout...
-            print(f"Target {t} has some HTTP Errors via http:// lets let https:// ", exception)
-        except requests.HTTPError as exception:
-            print(f"Target {t} has some HTTP Errors :--> ", exception)
+                results += f"<p style='color: green;'>Target: {t} is not Vulnerable</p>"
         except Exception as e:
-            print("Exception Occured with Description ----> ", e)
-            raise("Target Didn't Responsed")
-    print("All Targets Tested Successfully !!")
-except:
-    print("[*] Usage: python3 clickjackingByMealTeam6.py -f <file_name>")
-    print("[*] The Code might not worked for you , please retry & try --help option to know more")
-    exit(0)
+            results += f"<p>Error checking {t}: {str(e)}</p>"
+    return results
+
+if __name__ == '__main__':
+    app.run(debug=True)
